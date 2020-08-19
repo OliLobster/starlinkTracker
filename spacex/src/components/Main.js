8c30ff7f58f45a26e779522f920947ca0d0f3377
OliLobster <sgyr1993@163.com>
2020-08-18 21:28:58 -0700

    Add SatSetting component

--- spacex/src/components/Main.js
+++ spacex/src/components/Main.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
-
+import SatSetting from './SatSetting';
 class Main extends Component {
     render() {
         return (
             <div className='main'>
                 <div className="left-side">
-                    left
+                    <SatSetting />
                 </div>
                 <div className="right-side">
                     right
